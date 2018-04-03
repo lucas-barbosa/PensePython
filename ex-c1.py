a06ca3575adaddfa55bd395fcde674a2298e0bba
Lucas Barbosa <barbosa.lucash@gmail.com>
2018-04-03 20:08:31 -0300

    Add Exercise 1

---
+++ ex-c1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Ex 1.2.1 - Quantos segundos há em 42 minutos e 42 segundos?
+print(42 * 60 + 42)
+
+#  Ex 1.2.2 - Quantas milhas há em 10 quilômetros? Dica: uma milha equivale a 1,61 quilômetro.
+print(10 / 1.61)
+
+#  Ex 1.2.3 - Se você correr 10 quilômetros em 42 minutos e 42 segundos, qual é o seu passo médio (tempo por milha em minutos e segundos)?
+print((42 * 60 + 42) / (10 / 1.61) / 60)
+
+## Qual é a sua velocidade média em milhas por hora?
+print((10 / 1.61) * (60 * 60) / (42 * 60 + 42))
